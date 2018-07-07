dae47f310eb9df68763fbfcb0f9d72b1f34567e6
eckmLJE <lucas.eckman@gmail.com>
2018-07-07 18:24:40 -0400

    added post

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,6 +6,17 @@ module Api
         render json: Event.includes(:users), include: ["users"]
       end
 
+      def create
+        event = Event.create(event_params)
+        render json: event
+      end
+
+      private
+
+      def event_params
+        params.require(:event).permit(:id, :name, :venue, :date)
+      end
+
     end
   end
 end
