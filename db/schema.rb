1ba110d8342f0f4431c129719d9637cff19a813f
eckmLJE <lucas.eckman@gmail.com>
2018-07-10 10:29:28 -0400

    pre-auth working

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2018_07_06_171812) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-bob = User.create(name: "Bob")
+bob = User.create(username: "Bob")
 td = Event.create(name: "Tenacious D")
+
+td.users << bob
